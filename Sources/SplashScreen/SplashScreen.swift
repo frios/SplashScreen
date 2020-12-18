2b90a3f0a303bcd2d0819ab33b40df9b59ce2256
Ferdinand Rios <frios@viciouscircle.com>
2020-12-18 11:07:06 -0800

    Added duration and delay params removed unused code

--- Sources/SplashScreen/SplashScreen.swift
+++ Sources/SplashScreen/SplashScreen.swift
@@ -12,55 +12,29 @@ import SwiftUI
 public struct SplashScreen<Content: View>: View {
     
     @State private var showSplash = true
-
-    @State var percent = 0.0
-    @State var uScale: CGFloat = 1
     
+    let duration: Double
+    let delay: Double
     let content: Content
     
-    public init(@ViewBuilder contentProvider: () -> Content){
+    public init(duration: Double = 0, delay: Double = 0, @ViewBuilder contentProvider: () -> Content){
+        self.duration = duration
+        self.delay = delay
         self.content = contentProvider()
     }
     
     public var body: some View {
-        ZStack {
-            content
-        }
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-        .edgesIgnoringSafeArea(.all)
-        .opacity(showSplash ? 1 : 0)
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                withAnimation(.easeOut(duration: 1)) {
-                        self.showSplash = false
+        content
+            .edgesIgnoringSafeArea(.all)
+            .opacity(showSplash ? 1 : 0)
+            .zIndex(0) //push this screen to the back
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation(.easeOut(duration: duration)) {
+                            showSplash = false
+                    }
                 }
             }
-        }
-        .zIndex(0)
-    }
-}
-
-extension SplashScreen {
-    var uAnimationDuration: Double { return 1.0 }
-    
-    func handleAnimations() {
-        runAnimationPart1()
-        restartAnimation()
-    }
-    
-    func runAnimationPart1() {
-        withAnimation(.easeIn(duration: uAnimationDuration)) {
-            percent = 1
-            uScale = 5
-        }
-    }
-    
-    func restartAnimation() {
-        let deadline: DispatchTime = .now() + uAnimationDuration
-        DispatchQueue.main.asyncAfter(deadline: deadline) {
-            self.percent = 0
-            self.handleAnimations()
-        }
     }
 }
 
